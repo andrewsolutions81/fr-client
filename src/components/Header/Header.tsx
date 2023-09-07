b775d94f8582678d0a85bbf81bba0c99a585e497
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-09-06 23:52:51 -0500

    feat: Create pages and components folders, installed sass.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.styles.scss"
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="header__title">Header</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+// import React , { FC }from 'react'
+import React from 'react'
+import Header from '../../components/Header/Header'
+
+
+const Home = () => {
+  return (
+<div className="Home">
+  <Header />
+  <h1 className="home-h1">Home</h1>
+</div>  )
+}
+
+export default Home
+
