3ec04ae61144160c81f01ca8a87b606db669e575
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-09-26 14:27:43 -0500

    feat: create Favorites, User pages, add page route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,25 @@ import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
 import NotFound from './components/NotFound/NotFound';
+import Admin from './pages/Admin/Admin';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import Favorites from './pages/Favorites/Favorites';
+import User from './pages/User/User';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
       <Footer />
     </div>
