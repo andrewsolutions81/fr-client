f9e03b83aac78fdb18c5bdad4c4233e0b38f03d5
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-10-04 09:48:25 -0500

    feat: create LoginForm.

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -3,9 +3,32 @@ import "./LoginForm.style.scss"
 
 const LoginForm: React.FC = () => {
   return (
-    <form className="loginform">
-      <h1 className="loginform__title">Login Form</h1>
-    </form>
+    <div className="loginform">
+      <form action="" className="loginform__form">
+        <h1 className="loginform__title">Santa Barbara Inmobiliaria</h1>
+        <h2 className="loginform__iniciarsesion">Iniciar sesion </h2>
+        <div className="loginform__inputs--container">
+          <input
+            className="loginform__input loginform__input--email"
+            type="email"
+            placeholder="Correo electronico"
+          />
+          <input
+            className="loginform__input loginform__input--numero"
+            type="password"
+            placeholder="Contraseña"
+          />
+          <span className="loginform_error"> Error de credenciales</span>
+        </div>
+        <div className="loginform__actions">
+          <a className="loginform__login" href="google.com">
+            Sign up
+          </a>
+          <button className="loginform__btn">Ingresar</button>
+        </div>
+      </form>
+
+    </div>
   );
 };
 
