22a08a657c553627b40b3d01cb8f207c3cd95750
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-10-12 18:51:13 -0500

    fix: apiSignup working.

--- src/api/apiSignup.ts
+++ src/api/apiSignup.ts
@@ -3,24 +3,22 @@ import BASE_URL from "../services/apiConfig";
 
 export const apiSignup = async (user: signupInput) => {
   try {
-    const requestOptions = {
+    const options = {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(user),
     };
-    const response = await fetch(
-      `${BASE_URL}/auth/local/signup`,
-      requestOptions
-    );
-    console.log("requestOptions.body",requestOptions.body)
+
+    const response = await fetch(`${BASE_URL}/auth/local/signup`, options);
+
     if (!response.ok) {
       return new Error(`❌ apiSignup response.status: ${response.status}`);
     }
 
-    const res = await response.json();
-    console.log("res in apissignup: ",res);
-    return res.json()
-  } catch (error:any) {
+    return response.json();
+  } catch (error: any) {
     console.error("❌ apiSignup error :", error);
     return new Error(error);
   }
